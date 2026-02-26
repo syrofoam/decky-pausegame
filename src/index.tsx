20ad22dca3249f286792b520579917336b038861
Stian <44478082+syrofoam@users.noreply.github.com>
2026-02-26 22:03:57 +0100

    Change title style in plugin definition

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,16 @@ import {
   ButtonItem,
   definePlugin,
   ServerAPI,
-  Static,
 } from "decky-frontend-lib";
 import { FaPause } from "react-icons/fa";
 
 export default definePlugin((serverApi: ServerAPI) => {
   return {
-    title: <div className={Static.Title}>Quick Pause</div>,
+    title: <div style={{ fontWeight: "bold" }}>Quick Pause</div>,
     content: (
       <ButtonItem
         layout="below"
         onClick={async () => {
-           // We just call "toggle_game" and let Python figure out the rest
            await serverApi.callPluginMethod("toggle_game", {});
         }}
       >
